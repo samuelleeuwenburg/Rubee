2c27e145cb63812f1703a4d170bcab146686e01e
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2014-09-26 16:53:52 +0200

    fix path to sqlite db

--- plugins/karma.rb
+++ plugins/karma.rb
@@ -3,7 +3,7 @@ require "sequel"
 
 class Karma 
 	include Cinch::Plugin
-	DB = Sequel.connect("sqlite://"+File.dirname(__FILE__)+"/rubee.db")
+	DB = Sequel.sqlite(File.dirname(__FILE__)+"/rubee.db")
 
 	def renderKarma(nick)
 		nicks = DB[:karma]
