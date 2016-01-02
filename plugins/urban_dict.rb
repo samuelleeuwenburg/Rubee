8a916f76e00ddbdf79834fe195f51417440c5936
Milo de Vries <Milo de Vries>
2016-01-02 18:29:15 +0100

    Adds urban blacklist functionality (untested)

--- plugins/urban_dict.rb
+++ plugins/urban_dict.rb
@@ -6,20 +6,57 @@ require 'cgi'
 class Dictionary
   include Cinch::Plugin
 
+  def initialize(*)
+    super
+
+    @DB = Sequel.sqlite(File.dirname(__FILE__)+"/../rubee.db")
+  end
+
+  match(/^shut up(.+)/i, method: :shutUp, use_prefix: false)
+  def shutUp(m)
+    if message.downcase.include? @nick.downcase
+      niglet = getLastMessage
+      addNiggerString(niglet)
+    end
+  end
+
+  def isNiggerString?(string)
+    niggers = @DB[:urban_niggerlist]
+    niggers.where(:string => string).one?
+  end
+
+  def addNiggerString(string)
+    niggers = @DB[:urban_niggerlist]
+    niggers.insert(:string => string)
+  end
+
+  def getLastMessage
+    last = @DB[:urban_lastmessage]
+    last.first
+  end
+
+  def setLastMessage(message)
+    last = @DB[:urban_lastmessage]
+    last.update(:last => last, :message => message)
+  end
+
   match(/^(.+)\?\?\s*$/i, method: :urban_dict, use_prefix: false)
   match(/^\.u (.+)/i, method: :urban_dict, use_prefix: false)
   def urban_dict(m, query)
-    url        = "http://www.urbandictionary.com/define.php?term=#{CGI.escape(query)}"
-    definition = Nokogiri::HTML(open(url)).at("div.meaning").text.gsub(/\s+/, ' ')
-    limit = 220
 
-    unless definition.include? "There aren't any definitions"
-      if definition.length < limit
-        m.reply query + ': ' + definition
-      else
-        m.reply query + ': ' + definition[0..limit] + '...'
+    unless isNiggerString?(query)
+      url        = "http://www.urbandictionary.com/define.php?term=#{CGI.escape(query)}"
+      definition = Nokogiri::HTML(open(url)).at("div.meaning").text.gsub(/\s+/, ' ')
+      limit = 220
+
+      unless definition.include? "There aren't any definitions"
+        if definition.length < limit
+          m.reply query + ': ' + definition
+        else
+          m.reply query + ': ' + definition[0..limit] + '...'
+        end
+        setLastMessage(query)
       end
     end
   end
 end
-
